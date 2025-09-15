f656bfa49ff9db8f6c30609e3c569701deece546
nandu213jid <mcnandu0@gmail.com>
2025-09-15 15:11:29 +0530

    java

---
+++ Displaydate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DisplayDate {
+    public static void main(String[] args) {
+        // Create a Date object that represents the current date and time
+        Date currentDate = new Date();
+        
+        // Print the date
+        System.out.println("Current Date: " + currentDate);
+    }
+}
